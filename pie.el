f858f1f40ebb6d03f66d7af7b2529f5491266ad1
jao <jao@gnu.org>
2021-04-13 22:56:54 +0100

    basic REPL

--- pie.el
+++ pie.el
@@ -75,6 +75,7 @@
 ;;; Code:
 
 (require 'lisp-mode)
+(require 'subr-x)
 
 
 ;; Customization
@@ -142,7 +143,8 @@
   '("same" "symm" "cong" "replace" "trans" "ind-="))
 
 (defvar pie-font-lock-keywords
-  `(("'[a-zA-z-]+" . 'pie-atom)
+  `(("^#lang pie[ \t]*$" . font-lock-comment-face)
+    ("'[a-zA-z-]+" . 'pie-atom)
     ("(\\(define\\|the\\|claim\\)\\>" (1 'pie-definition))
     (,(concat "(" (regexp-opt pie-syntax-forms t)) (1 'pie-builtin))
     (,(regexp-opt (append pie-builtin-functions
@@ -236,6 +238,135 @@ Commands:
 
 
 ;; Repl
+
+(defcustom pie-path "pie"
+  "The path to your pie-hs compiled executable."
+  :type 'string)
+
+(defvar pie--last-pie-buffer nil)
+
+(defvar pie--repl-buffer "*pie*")
+(defun pie--repl-buffer () (get-buffer pie--repl-buffer))
+
+(defun pie-pop-to-repl ()
+  "Pop to the active Pie REPL."
+  (interactive)
+  (pop-to-buffer (pie--repl-buffer)))
+
+(defvar pie--out-buffer "*pie-output*")
+(defun pie--out-buffer ()
+  "Access to the raw pie-hs output."
+  (when (not (get-buffer pie--out-buffer))
+    (with-current-buffer (get-buffer-create pie--out-buffer)
+      (pie-mode)))
+  (get-buffer pie--out-buffer))
+
+(defun pie-back-to-buffer ()
+  "Return to the last Pie code buffer."
+  (interactive)
+  (when (get-buffer pie--last-pie-buffer)
+    (pop-to-buffer pie--last-pie-buffer)))
+
+(defun pie--send-command (cmd)
+  "Sends CMD to the Pie REPL and collects the result."
+  (when-let ((repl (pie--repl-buffer)))
+    (let ((proc (get-buffer-process repl))
+          (out (pie--out-buffer)))
+      (with-current-buffer out
+        (erase-buffer)
+        (comint-redirect-send-command-to-process cmd out proc nil t)
+        ;; Wait for the process to complete
+        (set-buffer repl)
+        (while (and (null comint-redirect-completed)
+		    (accept-process-output proc)))
+        (set-buffer out)
+        (string-trim (buffer-string))))))
+
+(defun pie-load-buffer (&optional buffer)
+  "Send the contents of a given buffer to the Pie repl, resetting it."
+  (interactive)
+  (let ((bn (buffer-file-name (or buffer (current-buffer)))))
+    (pop-to-buffer pie--repl-buffer)
+    (comint-simple-send nil (format ":load %s" bn))))
+
+(defun pie--eval (s)
+  "Asks the REPL to eval the string or form S."
+  (when s
+    (let* ((s (if (stringp s) s (format "%S" s)))
+           (s (replace-regexp-in-string "\\(;.*\\)?\n" " " s))
+           (res (pie--send-command (format "%s" s))))
+      (message "=> %s" res))))
+
+(defun pie-eval-last-sexp (&optional goto-repl)
+  "Sends to the Pie repl sexp before point for evaluation.
+
+With prefix, jump to the REPL afterwards."
+  (interactive "P")
+  (save-current-buffer
+    (let ((p (point)))
+      (save-excursion
+        (backward-sexp)
+        (pie--eval (buffer-substring-no-properties (point) p)))))
+  (when goto-repl (pie-pop-to-repl)))
+
+(defun pie-eval-region (&optional goto-repl)
+  "Sends to the Pie repl sexp before point for evaluation."
+  (interactive "P")
+  (let ((b (region-beginning))
+        (e (region-end)))
+    (pie--eval (buffer-substring-no-properties b e)))
+  (when goto-repl (pie-pop-to-repl)))
+
+(defun pie--sentinel (proc event)
+  "Prints a farewell when EVENT says PROC ended."
+  (let ((pb (process-buffer proc)))
+    (when (buffer-live-p pb)
+      (with-current-buffer pb
+        (comint-write-input-ring)
+        (insert "\nIt's been nice interacting with you!\n")))))
+
+;;;###autoload
+(define-derived-mode pie-repl-mode comint-mode "Pie REPL"
+  "A very simple comint-based mode to run pie-hs."
+  (setq comint-prompt-read-only t
+        comint-prompt-regexp "^.> "
+        comint-input-ring-file-name
+        (expand-file-name "~/.emacs.d/cache/pie.history")
+        comint-input-ignoredups t
+        comint-input-ring-size 10000)
+  (add-hook 'comint-dynamic-complete-functions #'pie-complete-symbol-at-point
+            nil t)
+  (add-hook 'completion-at-point-functions #'pie-complete-symbol-at-point t t)
+  (add-hook 'kill-buffer-hook #'comint-write-input-ring nil t)
+  (set-process-sentinel (get-buffer-process (current-buffer)) #'pie--sentinel)
+  (comint-read-input-ring t))
+
+(define-key pie-mode-map "\C-cz" #'pie-run)
+(define-key pie-mode-map "\C-c\C-z" #'pie-run)
+(define-key pie-mode-map "\C-cl" #'pie-load-buffer)
+(define-key pie-mode-map "\C-c\C-l" #'pie-load-buffer)
+(define-key pie-mode-map "\C-c\C-e" #'pie-eval-last-sexp)
+(define-key pie-mode-map "\C-x\C-e" #'pie-eval-last-sexp)
+(define-key pie-mode-map "\C-c\C-r" #'pie-eval-region)
+
+(define-key pie-repl-mode-map "\M-p" 'comint-previous-matching-input-from-input)
+(define-key pie-repl-mode-map "\M-n" 'comint-next-matching-input-from-input)
+(define-key pie-repl-mode-map "\C-cz" 'pie-back-to-buffer)
+(define-key pie-repl-mode-map "\C-c\C-z" 'pie-back-to-buffer)
+
+;;;###autoload
+(defun pie-run ()
+  "Run or switch to an existing Pie REPL."
+  (interactive)
+  (when (derived-mode-p 'pie-mode)
+    (setq pie--last-pie-buffer (current-buffer)))
+  (pop-to-buffer (make-comint "pie" pie-path))
+  (unless (derived-mode-p 'pie-repl-mode)
+    (pie-repl-mode)))
+
+(with-eval-after-load "company"
+  (when (listp company-global-modes)
+    (add-to-list 'company-global-modes 'pie-repl-mode)))
 
 
 (provide 'pie)
